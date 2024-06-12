b12fb9127ce51e3f08abf60d28f2d5479da58d98
lilmoon99 <oxygen4ik@bk.ru>
2024-06-12 11:43:43 +0400

    added documentation for book controller

--- lesson3/seminar3/src/main/java/ru/lilmoon/seminar3/api/BookController.java
+++ lesson3/seminar3/src/main/java/ru/lilmoon/seminar3/api/BookController.java
@@ -1,5 +1,12 @@
 package ru.lilmoon.seminar3.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.lilmoon.seminar3.entity.BookEntity;
@@ -17,6 +24,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/book")
+@Tag(name = "Books",description = "API to manage a books.")
 public class BookController {
     @Autowired
     private final BookServiceImpl service;
@@ -26,20 +34,52 @@ public class BookController {
     }
 
     @GetMapping
+    @Operation(
+            summary = "Retrieves an all books",
+            description = "Retrieves a list of books from Data Base."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(array = @ArraySchema(schema = @Schema(implementation = BookEntity.class)),mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404",content = {@Content(schema = @Schema())})
+    })
     public List<BookEntity> getAllBooks() {
         return service.getAll();
     }
 
+    @Operation(
+            summary = "Retrieves the book by :id",
+            description = "The response is book object with id,name."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = BookEntity.class),mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404",content = {@Content(schema = @Schema())})
+    })
     @GetMapping("/{id}")
     public BookEntity getBookById(@PathVariable long id) {
         return service.getById(id);
     }
 
+    @Operation(
+            summary = "Creates and adds a new book to DB",
+            description = "Creates a new book and adds it to the DB. Requires a json body."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", content = {@Content(schema = @Schema(implementation = BookEntity.class),mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404",content = {@Content(schema = @Schema())})
+    })
     @PostMapping
     public BookEntity addBook(@RequestBody BookEntity book) {
         return service.createBook(book);
     }
 
+    @Operation(
+            summary = "Deletes the book by :id",
+            description = "Deletes the book from DB by :id."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = BookEntity.class),mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404",content = {@Content(schema = @Schema())})
+    })
     @DeleteMapping("/{id}")
     public void deleteBookById(@PathVariable long id) {
         service.deleteBook(id);
