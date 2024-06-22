e5645586c976c33f147f3e7a970062b313cf9fbf
lilmoon99 <oxygen4ik@bk.ru>
2024-06-22 18:57:42 +0400

    hw 8 init commit

---
+++ spring-aop-hw/src/main/java/ru/lilmoon/spring_aop_hw/SpringAopHwApplication.java
@@ -0,0 +1,13 @@
+package ru.lilmoon.spring_aop_hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAopHwApplication.class, args);
+	}
+
+}
